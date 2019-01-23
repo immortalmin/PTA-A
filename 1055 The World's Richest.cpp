b8179f79f839d0a8e71aa980cb37403aaa66ccbe
immortalmin <1608725226@qq.com>
2019-01-23 23:29:33 +0800

    2019-1-23 hehehe

---
+++ 1055 The World's Richest.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+struct Info{
+	char name[20];
+	int age,money;
+}info;
+vector<Info>v1;
+bool mysort1(Info a,Info b){
+	if(a.money!=b.money) return a.money>b.money;
+	else if(a.age!=b.age) return a.age<b.age;
+	else return strcmp(a.name,b.name)<=0;//char数组排序 
+}
+int main(){
+//	freopen("1.txt","r",stdin);
+	int n,k;
+	scanf("%d %d",&n,&k);
+	for(int i=0;i<n;i++){
+		scanf("%s %d %d",&info.name,&info.age,&info.money);
+		v1.push_back(info);
+	}
+	sort(v1.begin(),v1.end(),mysort1);
+	int num,l,r;
+	for(int i=0;i<k;i++){
+		scanf("%d %d %d",&num,&l,&r);
+		printf("Case #%d:\n",i+1);
+		int flag=0;
+		for(int j=0;j<v1.size();j++){
+			if(v1[j].age>=l&&v1[j].age<=r){
+				flag=1;
+//				strcpy(c,v1[j].name.c_str());//string转char数组 
+				printf("%s %d %d\n",v1[j].name,v1[j].age,v1[j].money);
+				num--;
+				if(num==0) break;
+			}
+		}
+		if(flag==0){
+			printf("None\n");
+			continue;
+		}
+	}
+	return 0;
+}
---
+++ 1085 Perfect Sequence.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstdio>
+using namespace std;
+vector<int>v;
+int main(){
+//	freopen("1.txt","r",stdin);
+	int n,m,p,a;
+	scanf("%d %d",&n,&p);
+	for(int i=0;i<n;i++){
+		scanf("%d",&a);
+		v.push_back(a);
+	}
+	int sum=0,max=0,k=v.size()-1;
+	sort(v.begin(),v.end());
+	for(int i=0;i<v.size();i++){
+		if(v[i]<=v[0]*p) sum++;
+		else{
+			k=i;
+			break;
+		}
+	}
+	max=sum;
+	for(int i=1;i<v.size();i++){
+		sum--;
+		for(int j=k;j<v.size();j++){
+			if(v[j]>v[i]*p){
+				k=j;
+				break;
+			}
+			sum++;
+		}
+		if(sum>max) max=sum;
+	}
+	cout<<max<<endl;
+	return 0;
+}
